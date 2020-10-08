0180f193451d7af2038694d33d8be31fd01a4d14
Danny van Wijk <danny@insiders.nl>
2020-10-08 10:26:26 +0000

    Change media path usage

--- src/Kunstmaan/MediaBundle/Helper/File/FileHandler.php
+++ src/Kunstmaan/MediaBundle/Helper/File/FileHandler.php
@@ -225,7 +225,7 @@ class FileHandler extends AbstractMediaHandler
 
         $media->setContentType($contentType);
         $media->setFileSize(filesize($media->getContent()));
-        $media->setUrl($this->mediaPath.$this->getFilePath($media));
+        $media->setUrl($this->getFilePath($media));
         $media->setLocation('local');
     }
 
@@ -355,7 +355,8 @@ class FileHandler extends AbstractMediaHandler
         }
 
         return sprintf(
-            '%s/%s',
+            '%s%s/%s',
+            $this->mediaPath,
             $media->getUuid(),
             $filename
         );
@@ -368,7 +369,9 @@ class FileHandler extends AbstractMediaHandler
      */
     private function getFileFolderPath(Media $media)
     {
-        return substr($this->getFilePath($media), 0, strrpos($this->getFilePath($media), $media->getOriginalFilename()));
+        $filePath = $this->getFilePath($media);
+
+        return substr($filePath, 0, strrpos($filePath, $media->getOriginalFilename()));
     }
 
     private function guessMimeType($pathName)
