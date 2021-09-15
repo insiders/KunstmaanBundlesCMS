282c463e5776028ef45f56655ce6e5031a86f362
waaghals <patrick.stroef@gmail.com>
2021-09-15 10:38:38 +0200

    KUN-6636 Added event to populate robots.txt
    * KUN-6636 Added event to populate robots.txt
    
    * KUN-6636 Code review fixes
    
    * KUN-6636 Do not mark value objects as final
    
    * KUN-6636 Make RobotsEvent final
    
    Co-authored-by: Patrick Berenschot <patrick@insiders.nl>

--- src/Kunstmaan/SeoBundle/Controller/RobotsController.php
+++ src/Kunstmaan/SeoBundle/Controller/RobotsController.php
@@ -2,17 +2,23 @@
 
 namespace Kunstmaan\SeoBundle\Controller;
 
-use Kunstmaan\SeoBundle\Entity\Robots;
+use Kunstmaan\SeoBundle\Event\RobotsEvent;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class RobotsController extends Controller
 {
+    private $dispatcher;
+
+    public function __construct(EventDispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
     /**
-     * Generates the robots.txt content when available in the database and falls back to normal robots.txt if exists
-     *
      * @Route(path="/robots.txt", name="KunstmaanSeoBundle_robots", defaults={"_format": "txt"})
      * @Template(template="@KunstmaanSeo/Admin/Robots/index.html.twig")
      *
@@ -20,18 +26,10 @@ class RobotsController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $entity = $this->getDoctrine()->getRepository(Robots::class)->findOneBy([]);
-        $robots = $this->getParameter('robots_default');
+        $event = new RobotsEvent();
 
-        if ($entity && $entity->getRobotsTxt()) {
-            $robots = $entity->getRobotsTxt();
-        } else {
-            $file = $request->getBasePath() . 'robots.txt';
-            if (file_exists($file)) {
-                $robots = file_get_contents($file);
-            }
-        }
+        $event = $this->dispatcher->dispatch($event);
 
-        return ['robots' => $robots];
+        return ['robots' => $event->getContent()];
     }
 }
