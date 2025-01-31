80b56ab703aeb23462da555966a05bf3de038605
Jeroen Thora <jeroenthora@gmail.com>
2025-01-31 22:10:41 +0100

    Merge pull request #3432 from insiders/url-chooser

--- src/Kunstmaan/NodeBundle/Form/EventListener/URLChooserFormSubscriber.php
+++ src/Kunstmaan/NodeBundle/Form/EventListener/URLChooserFormSubscriber.php
@@ -10,8 +10,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Url;
 
+/**
+ * @deprecated since 7.2 and will be removed in 8.0. Switch to the improved url chooser instead.
+ */
 class URLChooserFormSubscriber implements EventSubscriberInterface
 {
     use URLValidator;
